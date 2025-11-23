a034e2336d5b5b0e3ebbbcab693b257fc8f3dba0
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-23 22:43:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,22 +6,35 @@ const messages = {
   fr: "Ceci est un exemple de préférence de langue."
 };
 
-const saved = sessionStorage.language;
+let saved = sessionStorage.language;
+
 if (saved && messages[saved]) {
   messageEl.textContent = messages[saved];
+} else {
+  sessionStorage.language = "en";
+  messageEl.textContent = messages.en;
+}
+
+function ensureIndexHtml() {
+  if (!location.pathname.endsWith("index.html")) {
+    location.href = "index.html";
+  }
 }
 
 document.getElementById("en").addEventListener("click", () => {
   sessionStorage.language = "en";
   messageEl.textContent = messages.en;
+  ensureIndexHtml();
 });
 
 document.getElementById("es").addEventListener("click", () => {
   sessionStorage.language = "es";
   messageEl.textContent = messages.es;
+  ensureIndexHtml();
 });
 
 document.getElementById("fr").addEventListener("click", () => {
   sessionStorage.language = "fr";
   messageEl.textContent = messages.fr;
+  ensureIndexHtml();
 });
